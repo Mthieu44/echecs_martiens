8dfc49d1b95f22824f34a76d751174056924a9fe
E212406B <pacome.cailleteau@etu.univ-nantes.fr>
2022-06-14 17:54:04 +0200

    import

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -9,6 +9,8 @@ import javafx.scene.layout.Pane
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 import projet.echecmartien.controleur.ControleurBoutonSave
+import projet.echecmartien.controleur.ControleurBoutonValider
+import projet.echecmartien.controleur.ControleurCheckboxIA
 import projet.echecmartien.controleur.ControleurClicCase
 import projet.echecmartien.modele.Jeu
 import projet.echecmartien.modele.Joueur
